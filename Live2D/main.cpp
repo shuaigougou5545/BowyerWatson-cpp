35b4713ca28100b96b44f0dbe729e6139fde76cd
goujiayuan <737224178@qq.com>
2023-05-21 13:21:58 +0800

    Initial Commit

---
+++ Live2D/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Live2D
+//
+//  Created by 苟嘉远 on 2023/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
